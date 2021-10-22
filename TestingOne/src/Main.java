3700b71d762293d77a4cbdd8de9911fa4f2703e8
Tharusha Rukshan <82650047+tharushaRukshan2002@users.noreply.github.com>
2021-10-22 19:55:21 +0530

    second commit

--- TestingOne/src/Main.java
+++ TestingOne/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Tharusha");
+        System.out.println("Chamudi");
+        System.out.println("Rahsmi");
+        System.out.println("Tharushika");
 
 
     }
