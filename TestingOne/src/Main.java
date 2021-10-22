3312190a6e990bcd53f9b21a78f07dc33d56ae59
chamudiabeysinghe <chamudi.20200489@iit.ac.lk>
2021-10-22 20:25:47 +0530

    chamudi

--- TestingOne/src/Main.java
+++ TestingOne/src/Main.java
@@ -3,9 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Tharusha");
-        System.out.println("Chamudi");
-        System.out.println("Rahsmi");
-        System.out.println("Tharushika");
+        System.out.println("Chamudi-online");
 
 
     }
